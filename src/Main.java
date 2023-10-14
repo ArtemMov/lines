31fd356c34d129391fdfe023627557b43541a87e
Rabota <wulf1205@gmail.com>
2023-10-14 17:58:36 +0700

    stroki сделанно

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника " + fullName);
+
+
+        String upperFullName = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета - "+ upperFullName);
+
+        fullName = "Ёжиков Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        fullName = fullName.replace("Ё","Е");
+        System.out.println("Данные Ф. И. О. сотрудника — "+ fullName);
+
     }
+
 }
